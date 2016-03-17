1d6150386e2edd7ca13383c1cb21e8924f7f32cf
Grammos <kamberigramos@gmail.com>
2016-03-17 16:15:31 +0100

    Created the first browser, using Java! It's a simple one, and in order to use, please use the URL form as: http://yourpage.com enjoy it!

---
+++ simpleWebBrowser/src/simpleWebBrowser/ReadFileMain.java
@@ -0,0 +1,12 @@
+package simpleWebBrowser;
+import javax.swing.*;
+
+public class ReadFileMain {
+
+	public static void main(String[] args) {
+		
+		Browser readNow = new Browser();
+		readNow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
